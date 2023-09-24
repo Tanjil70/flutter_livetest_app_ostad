c78352afc503297d490fe29dd19acfd2d6643dd0
Tanjil70 <tanjiluits70@gmail.com>
2023-09-24 22:40:15 +0600

    Assignment Of Module 6 Submitted

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+/*
 import 'package:flutter/material.dart';
+
+import 'card.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +14,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  elevation: 10,
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white70,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0))))),
       home: MyHomePage(),
     );
   }
@@ -25,14 +34,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> imagesList = [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjGEIKf5RT-lxMxx_qOrK4opqQMOecvvGn9w&usqp=CAU',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/RedCat_8727.jpg/1200px-RedCat_8727.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAtxqSrBSeFoj-zXpQdUjxqhLJU86mkhndwg&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThddPBu4Y7Z_msmV_fE_HXLS5QG4IXJaj5aw&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5ZNus26cImi0EkSyzjxSIKjgILsc8lbExjg&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU4cXYbxLC5LU7i-9tjPQBxSAszrI1ZJCctg&usqp=CAU'
+  List<Product> fruitsList = [
+    Product(name: "Mango", price: "100"),
+    Product(name: "Banana", price: "50"),
+    Product(name: "Orange", price: "75"),
+    Product(name: "Apple", price: "100"),
+    Product(name: "Grapes", price: "200"),
+    Product(name: "Strawberry", price: "95"),
+    Product(name: "Watermelon", price: "60"),
+    Product(name: "Cherry", price: "85"),
+    Product(name: "Peach", price: "120"),
+    Product(name: "Pineapple", price: "110"),
   ];
+  int totalBought = 0;
+  void addCart(index) {
+    setState(() {
+      fruitsList[index].quantity++;
+      if (fruitsList[index].quantity == 5) {
+        _showCongratulationsDialog(fruitsList[index].name);
+      }
+      totalBought++;
+    });
+  }
+
+  void _showCongratulationsDialog(String productName) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('congratulations!'),
+            content: Text(
+              'You\'ve bought 5 $productName!',
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"))
+            ],
+          );
+        });
+  }
+
   mySnackBar(message, contex) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       content: Text(message),
@@ -44,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Photo Gallery"),
+        title: Text("Product List"),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white70,
         centerTitle: true,
@@ -52,84 +96,212 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Padding(
         padding: EdgeInsets.all(8.0),
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8),
-                child: Text(
-                  "Well Come to My Photo Gallery!",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 8, 0, 8),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search for Photos",
-                    border: OutlineInputBorder(),
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            ListView.builder(
+              itemCount: fruitsList.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("${fruitsList[index].name}"),
+                  subtitle: Text("${fruitsList[index].price} tk"),
+                  trailing: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text("Count ${fruitsList[index].quantity}"),
+                      ElevatedButton(
+                          onPressed: () {
+                            addCart(index);
+                          },
+                          child: Text("Buy Now"))
+                    ],
                   ),
-                ),
+                );
+              },
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CartPage(
+                              totalBought: totalBought,
+                            )));
+              },
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.blue,
               ),
-              GridView.builder(
-                  itemCount: imagesList.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      mainAxisSpacing: 5,
-                      crossAxisSpacing: 5,
-                      crossAxisCount: 3),
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                        onTap: () {
-                          mySnackBar("You Selected Cat No $index", context);
-                        },
-                        child: ListTile(
-                          title: Image.network(
-                            imagesList[index],
-                            height: 100,
-                            width: 150,
-                            fit: BoxFit.fill,
-                          ),
-                          subtitle: Text("Photo $index"),
-                        ));
-                  }),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-                child: Column(
-                  children: [
-                    ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: 3,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage: NetworkImage(imagesList[index]),
-                          ),
-                          title: Text("Description for Photo ${index + 1}"),
-                        );
-                      },
-                    )
-                  ],
-                ),
+            )
+          ]),
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Product {
+  String name;
+  String price;
+  int quantity = 0;
+  Product({required this.name, required this.price});
+}
+*/
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Product> fruitsList = [
+    Product(name: "Mango", price: "100"),
+    Product(name: "Banana", price: "50"),
+    Product(
+        name: "Mango", price: "150"), // Another Mango with a different price
+    Product(name: "Apple", price: "100"),
+  ];
+
+  Map<String, int> cart = {}; // Key: Unique identifier, Value: Count
+
+  void addCart(index) {
+    final product = fruitsList[index];
+    final uniqueIdentifier = '${product.name}${product.price}';
+
+    setState(() {
+      // Check if the cart map is initialized and not null
+      cart[uniqueIdentifier] = (cart[uniqueIdentifier] ?? 0) + 1;
+
+      if (cart[uniqueIdentifier] == 5) {
+        _showCongratulationsDialog(product.name);
+      }
+    });
+  }
+
+  void _showCongratulationsDialog(String productName) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Congratulations!'),
+          content: Text('You\'ve bought 5 $productName!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product List"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListView.builder(
+                itemCount: fruitsList.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  final product = fruitsList[index];
+                  final uniqueIdentifier = '${product.name}${product.price}';
+                  final count = cart[uniqueIdentifier] ?? 0;
+
+                  return ListTile(
+                    title: Text("${product.name}"),
+                    subtitle: Text("${product.price} tk"),
+                    trailing: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text("Count: $count"),
+                        ElevatedButton(
+                          onPressed: () {
+                            addCart(index);
+                          },
+                          child: Text("Buy Now"),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 8, 0, 8),
-                child: Center(
-                    child: ElevatedButton(
-                        onPressed: () {
-                          mySnackBar("Image Upload Sucessfully", context);
-                        },
-                        child: Icon(Icons.upload),
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blue,
-                            foregroundColor: Colors.white,
-                            shape: CircleBorder()))),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CartPage(
+                        cart: cart,
+                        fruitsList: fruitsList,
+                      ),
+                    ),
+                  );
+                },
+                child: Text("Go to Cart"),
               ),
             ],
           ),
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Product {
+  String name;
+  String price;
+  Product({required this.name, required this.price});
+}
+
+class CartPage extends StatelessWidget {
+  final Map<String, int> cart;
+  final List<Product> fruitsList;
+
+  CartPage({required this.cart, required this.fruitsList});
+
+  @override
+  Widget build(BuildContext context) {
+    // Calculate the total number of unique products in the cart
+    int totalUniqueProducts = cart.keys.length;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Total Unique Products: $totalUniqueProducts'),
+            // Display other cart items here
+          ],
+        ),
+      ),
     );
   }
 }
