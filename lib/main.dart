473f1fe634ed7c249a696fb74dbac79fa04046ae
Tanjil70 <tanjiluits70@gmail.com>
2023-09-25 10:37:48 +0600

    Assignment_of_Module_7 Submitted

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,14 @@
-/*
 import 'package:flutter/material.dart';
-
-import 'card.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-          elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                  elevation: 10,
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.white70,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(0))))),
       home: MyHomePage(),
     );
   }
@@ -42,139 +28,6 @@ class _MyHomePageState extends State<MyHomePage> {
     Product(name: "Grapes", price: "200"),
     Product(name: "Strawberry", price: "95"),
     Product(name: "Watermelon", price: "60"),
-    Product(name: "Cherry", price: "85"),
-    Product(name: "Peach", price: "120"),
-    Product(name: "Pineapple", price: "110"),
-  ];
-  int totalBought = 0;
-  void addCart(index) {
-    setState(() {
-      fruitsList[index].quantity++;
-      if (fruitsList[index].quantity == 5) {
-        _showCongratulationsDialog(fruitsList[index].name);
-      }
-      totalBought++;
-    });
-  }
-
-  void _showCongratulationsDialog(String productName) {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('congratulations!'),
-            content: Text(
-              'You\'ve bought 5 $productName!',
-            ),
-            actions: [
-              TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text("OK"))
-            ],
-          );
-        });
-  }
-
-  mySnackBar(message, contex) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text(message),
-      //backgroundColor: Colors.green,
-    ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Product List"),
-        backgroundColor: Colors.blue,
-        foregroundColor: Colors.white70,
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            ListView.builder(
-              itemCount: fruitsList.length,
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text("${fruitsList[index].name}"),
-                  subtitle: Text("${fruitsList[index].price} tk"),
-                  trailing: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text("Count ${fruitsList[index].quantity}"),
-                      ElevatedButton(
-                          onPressed: () {
-                            addCart(index);
-                          },
-                          child: Text("Buy Now"))
-                    ],
-                  ),
-                );
-              },
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => CartPage(
-                              totalBought: totalBought,
-                            )));
-              },
-              icon: Icon(
-                Icons.shopping_cart,
-                color: Colors.blue,
-              ),
-            )
-          ]),
-        ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class Product {
-  String name;
-  String price;
-  int quantity = 0;
-  Product({required this.name, required this.price});
-}
-*/
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Product> fruitsList = [
-    Product(name: "Mango", price: "100"),
-    Product(name: "Banana", price: "50"),
-    Product(
-        name: "Mango", price: "150"), // Another Mango with a different price
-    Product(name: "Apple", price: "100"),
   ];
 
   Map<String, int> cart = {}; // Key: Unique identifier, Value: Count
